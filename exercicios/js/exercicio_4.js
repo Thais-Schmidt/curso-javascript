843cdc51637fa12d482e928d147e9bfec4735052
Thais Schmidt <thaisachmidtdasilva@gmail.com>
2023-04-23 21:48:22 -0300

    fiz 3 exercicios e o css das paginas

---
+++ exercicios/js/exercicio_4.js
@@ -0,0 +1,48 @@
+//alert("Bem vindo!!");
+
+function resultados(){
+
+var arrayA = new Array(19);
+arrayA[0] = parseInt (document.getElementById("1").value);
+arrayA[1] = parseInt (document.getElementById("2").value);
+arrayA[2] = parseInt (document.getElementById("3").value);
+arrayA[3] = parseInt (document.getElementById("4").value);
+arrayA[4] = parseInt (document.getElementById("5").value);
+arrayA[5] = parseInt (document.getElementById("6").value);
+arrayA[6] = parseInt (document.getElementById("7").value);
+arrayA[7] = parseInt (document.getElementById("8").value);
+arrayA[8] = parseInt (document.getElementById("9").value);
+arrayA[9] = parseInt (document.getElementById("10").value);
+arrayA[10] =parseInt (document.getElementById("11").value);
+arrayA[11] =parseInt (document.getElementById("12").value);
+arrayA[12] =parseInt (document.getElementById("13").value);
+arrayA[13] =parseInt (document.getElementById("14").value);
+arrayA[14] =parseInt (document.getElementById("15").value);
+arrayA[15] =parseInt (document.getElementById("16").value);
+arrayA[16] =parseInt (document.getElementById("17").value);
+arrayA[17] =parseInt (document.getElementById("18").value);
+arrayA[18] =parseInt (document.getElementById("19").value);
+arrayA[19] =parseInt (document.getElementById("20").value);
+
+document.getElementById("1b").value = arrayA[19];
+document.getElementById("2b").value = arrayA[18];
+document.getElementById("3b").value = arrayA[17];
+document.getElementById("4b").value = arrayA[16];
+document.getElementById("5b").value = arrayA[15];
+document.getElementById("6b").value = arrayA[14];
+document.getElementById("7b").value = arrayA[13];
+document.getElementById("8b").value = arrayA[12];
+document.getElementById("9b").value = arrayA[11];
+document.getElementById("10b").value =arrayA[10];
+document.getElementById("11b").value =arrayA[9];
+document.getElementById("12b").value =arrayA[8];
+document.getElementById("13b").value =arrayA[7];
+document.getElementById("14b").value =arrayA[6];
+document.getElementById("15b").value =arrayA[5];
+document.getElementById("16b").value =arrayA[4];
+document.getElementById("17b").value =arrayA[3];
+document.getElementById("18b").value =arrayA[2];
+document.getElementById("19b").value =arrayA[1];
+document.getElementById("20b").value =arrayA[0];
+
+}
