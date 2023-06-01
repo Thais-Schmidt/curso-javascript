4321a19a950b86389911fca69ce32dd821eb57ad
Thais Schmidt <thaisachmidtdasilva@gmail.com>
2023-05-31 21:31:32 -0300

    aprendendo while

--- js/fatorial-com-for.js
+++ js/fatorial-com-for.js
@@ -1,13 +1,17 @@
-function fatorial(){
-    let num = parseInt (document.getElementById('num').value);
-    let numero = 1;
-
-    for(let i=num; i>0; i--){
-        numero = numero * i;
-    }
-    
-    document.getElementById("resultado").innerHTML = numero;
-
-}
+ function fatorial(){
+     let num = parseInt (document.getElementById('num').value);
+     let numero = 1
+     for(let i=num; i>0; i--){
+         numero = numero * i;
+     }  
+     document.getElementById("resultado").innerHTML = numero
+ }
 
 // O fatorial de um número é o produto dele pelos seus antecessores maiores que 0. O fatorial de um número é a multiplicação desse número por todos os seus antecessores maiores que zero.
+
+for(let i=1; i<9; i++){
+    if (i === 5){
+        break;
+    }
+    console.log('numero total de carros '+i);
+}
